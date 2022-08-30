073b250b81eb2097371e40bce6a207f86f04558d
Rod Salvador <39975851+jayr1125@users.noreply.github.com>
2022-08-30 10:33:03 +0800

    Update test.py
    reverted back to superfast to check if app will still crash

--- test.py
+++ test.py
@@ -317,7 +317,7 @@ try:
                 frequency='infer',
                 prediction_interval=0.95,
                 ensemble=None,
-                model_list='fast',
+                model_list='superfast',
                 max_generations=10,
                 num_validations=2,
                 no_negatives=True
